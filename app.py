432c7bbe86123117ede0cb2be0fe2d24dc42ab5c
Davis Knowles <dknowles52@gmail.com>
2021-05-01 14:38:13 -0400

    flask object connected to logic js

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 import datetime as dt
 
@@ -34,7 +34,7 @@ app = Flask(__name__)
 @app.route("/")
 def generate():
     """List all available api routes."""
-    dataset2 = "The future of green economy"
+    dataset2 = "EVLytics"
     return render_template('index.html', title=dataset2, data=ResultSet)
 
 @app.route("/get_data")
@@ -42,77 +42,15 @@ def query_db():
     query = db.select([commodities_tb])
     ResultProxy = connection.execute(query)
     ResultSet = ResultProxy.fetchall()
-    # result_dict = {}
-    # for position, value in enumerate(ResultSet):
-    #     result_dict.update({str(position):value})
-    # query_result = json.dumps(ResultSet[0])
-
-    # print(query_result)
-
-    # print(result_dict)
-    # return query_result  
+     
     return jsonify({'output': [dict(data) for data in ResultSet]})
+
+
+@app.route("/group")
+def group():
+    com = request.args.get('commodities')
+    return render_template('com')
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# @app.route("/api/v1.0/precipitation")
-# def precipitation():
-# # Create our session (link) from Python to the DB
-#     session = Session(engine)
-# #Find the most recent date in the data set
-# latest_date_str = session.query(Measurement).order_by(Measurement.date.desc()).first().date
-# latest_date = dt.datetime.strptime(latest_date_str, '%Y-%m-%d')
-# one_year_ago_date = latest_date - dt.timedelta(days = 365)
-# # Design a query to retrieve the last 12 months of precipitation data and plot the results. 
-# # Starting from the most recent data point in the database. 
-# results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= one_year_ago_date).all()
-
-# session.close()
-
-# # Convert the query results to a dictionary using `date` as the key and `prcp` as the value.
-# all_precip = []
-# for date, prcp in results:
-#     precip_dict = {}
-#     precip_dict["date"] = prcp
-#     all_precip.append(precip_dict)
-# return jsonify(all_precip)
-
-# @app.route("/api/v1.0/stations")
-# # Return a JSON list of stations from the dataset.
-# session = Session(engine)
-# # query for stations 
-# stations = session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).all()
-# session.close()
-
-# # convert results to dictionary 
-
-# all_stations = []
-
-# for station, name, latitude, longitude, elevation in stations:
-#     station_dict = {}
-#     station_dict["station"] = station
-#     station_dict["name"] = name
-#     station_dict["latitude"] = latitude
-#     station_dict["longitude"] = longitude
-#     station_dict["elevation"] = elevation
-#     all_stations.append(station_dict)
-# return jsonify(all_stations)
-
-# @app.route("/api/v1.0/tobs")
-# # Query the dates and temperature observations of the most active station for the last year of data.
-# session = Session(engine)
-# # query temps for most active station
-# station_db = session.query(Measurement.tobs).filter(Measurement.station == 'USC00519281').all()
-# session.close()
-
-# # convert to dictionary
-# all_temps = []
-# for date, temp in station_db:
-#     temp_dict = {}
-#     temp_dict[date] = temp
-#     all_temps.append(temp_dict)
-# return jsonify(all_temps)
-
-
 
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,75 +1,48 @@
 // Create a map object
 var myMap = L.map("mapid", {
     center: [15.5994, -28.6731],
-    zoom: 2
-});
+    zoom: 3
+})
   
 //   Adding tile layer
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
    tileSize: 512,
-   maxZoom: 13,
+   maxZoom: 10,
    zoomOffset: -1,
-   id: "mapbox/streets-v11",
+   id: "mapbox/light-v10",
    accessToken: API_KEY
 }).addTo(myMap);
 
-var link= "static/data/commodity.json";
+// Flask object 
+var link= "http://127.0.0.1:5000/get_data";
 
 
-//create map saed on qtty
-d3.json(link).then(function(data) {
+// // // Build Map
+// d3.json(link).then(function(data) {
+//   // var data = data.output
 
-  for (var i = 0; i < data.length; i++) {
+//   // Loop through the flask object and plot each coordinate
+//   for (var i = 0; i < data.length; i++) {
+
+//     L.marker([data[i].latitude, data[i].longitude], {
     
+//     }).bindPopup("<h3>" + data[i].fac_name +
+//     "</h3><hr><p>" + data[i].capacity + "</p>").addTo(myMap);
+//   }
+// });
 
-     commodity= data[i].commodity
-     if(data[i].capacity< 500 &&  data[i].capacity > 0){
-      var color_= "green" 
-   }
-   else if (data[i].capacity< 1000 &&  data[i].capacity > 500){
-       var color_= "#7FFF00" 
-   }
-   else if (data[i].capacity< 10000 &&  data[i].capacity > 1000){
-       var color_= "yellow" 
-   }
-   else if (data[i].capacity< 100000 &&  data[i].capacity > 10000){
-       var color_= "#DEB887" 
-   }
-   else if (data[i].capacity< 200000 &&  data[i].capacity > 100000){
-       var color_= "orange" 
-   }
-   else {
-       var color_= "red"
-   }
-
-        var place= [data[i].latitude, data[i].longitude]
-        var markers= L.circle(place, {
-          fillOpacity: 0.75,
-          color: color_,
-          fillColor: color_,
-          radius: 10000
-
-        })
-        .bindPopup(data[i].fac_name).addTo(myMap);
-    
-
-  
-
-      }
- 
-  
-});
-
-  var commodity_name= ["Nickel", "Cobalt","Lithium","Coal","Aluminum","Copper"];
-  var data= "static/data/commodity.csv";
-  var financial= "static/data/financial.json";
-  var linkin= "static/data/commodity.json";
+var commodity_name= ["Nickel", "Cobalt","Lithium","Coal","Aluminum","Copper"];
+var data= "static/data/commodity.csv";
+var financial= "static/data/financial.json";
  
 function buildgraph(index){
+
+  // Build Public/Private window
   var public=[]
   var private=[]
-  d3.json(linkin).then(function(dataset) {
+  d3.json(link).then(function(data) {
+    console.log(data.object)
     var data_info = dataset.map(function(dataset){
     return {"Commodity":dataset.commodity, "Type":dataset.Company_Type};
     });
@@ -89,56 +62,38 @@ function buildgraph(index){
     var demo_info= d3.select('#sample-metadata')
     demo_info.html('')
     
-      var info= demo_info.append('p').text(`Public Company: ${comp[index].values[0].values.length} `)
-      var re_nfo= demo_info.append('p').text(`Private Company : ${comp[index].values[1].values.length} `)
+    var info= demo_info.append('p').text(`Public Company: ${comp[index].values[0].values.length} `)
+    var re_nfo= demo_info.append('p').text(`Private Company : ${comp[index].values[1].values.length} `)
    
-    
-
   });
   
-
-    d3.csv(data).then(function(info){
+  // Build commodity financial graph  
+  d3.csv(data).then(function(info){
       // console.log(info)
-      
-      for (var i=0; i< commodity_name.length; i++){
-        
-        
-        var information= d3.select('#selDataset').append('option').text(commodity_name[i])
-       
-      };
-      var month_=[];
+    for (var i=0; i< commodity_name.length; i++){
+      var information= d3.select('#selDataset').append('option').text(commodity_name[i])
+    }
+    var month_=[];
   
-      var comodity= d3.nest()
-        .key(function(d) { return d.Commodity; })
-        
-        
-        
-        .entries(info);
-      
-      
-      var price_=[]; 
-      
-        var como_info= comodity[index].key
-        var como_index= comodity[index]
-        for (var i=0; i< como_index.values.length; i++){
-          var values= como_index.values[i].Month
-          var priz= parseInt(como_index.values[i].Price.replace(/,/g, ''), 10)
-        
-          month_.push(values)
-          price_.push(priz)
-        } 
+    var comodity= d3.nest()
+      .key(function(d) { return d.Commodity; })
+      .entries(info);
+    var price_=[]; 
+    var como_info= comodity[index].key
+    var como_index= comodity[index]
 
-      
-
-      var month=[];
-      var price=[];
-    
-      
-      
-      
-      var chart = new Chart('chart', {
+    for (var i=0; i< como_index.values.length; i++){
+      var values= como_index.values[i].Month
+      var priz= parseInt(como_index.values[i].Price.replace(/,/g, ''), 10)
+        
+      month_.push(values)
+      price_.push(priz)
+    } 
+    var month=[];
+    var price=[];
+    var chart = new Chart('chart', {
         type: 'line',
-      data: {
+        data: {
         labels: month_,
         datasets: [
           {
@@ -149,17 +104,12 @@ function buildgraph(index){
             }
           ]
         }
-      });
-
-    
-  
+    });
   });
   
-    
- 
+};buildgraph(0);
 
-};
-buildgraph(0);
+
 function handlesubmit(){
   var data= "static/data/commodity.csv";
   var link= "static/data/commodity.json";
@@ -219,8 +169,8 @@ function handlesubmit(){
 
 d3.select('#selDataset').on('change', handlesubmit);
 
-var trying = d3.csv.parse(d3.select('commodity.csv').text());
-console.log(trying);
+// var trying = d3.csv.parse(d3.select('commodity.csv').text());
+// console.log(trying);
 
   
   
